520cfd7fa4b76e7ee413c82f129a264ca0aabf1e
Bian Hengwei <HBIAN001@e.ntu.edu.sg>
2019-10-11 15:13:13 +0800

    deleted redundant files and writed some of the gui

---
+++ FS1_Bian_Lim_Cheng.py
@@ -0,0 +1,28 @@
+from tkinter import *
+
+
+# draws the cover
+def cover(frame):
+    frame.place(relheight=600, relwidth=400)
+    background = PhotoImage(file='images/background.png')
+    background_label = Label(frame, image=background)
+    background_label.place(x=0, y=0)
+
+
+# main func that runs the whole app
+def main():
+    # creates a root obj and its frame
+    root = Tk()
+    root.geometry('600x400')
+    root.resizable(True, True)
+    frame = Frame(root)
+
+    # draws the cover
+    cover(frame)
+
+    # launch the app and block until window is closed
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
