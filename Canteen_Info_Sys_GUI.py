1976bcb23cf59d16cae6e53af715c637d9d37d62
bian <qiong@Harry-Bians-Macbook.local>
2019-10-15 17:48:46 +0800

    updated pages_template function

--- Canteen_Info_Sys_GUI.py
+++ Canteen_Info_Sys_GUI.py
@@ -130,43 +130,40 @@ def error(root, data, error_message):
 
 # this is a page template
 # copy and paste when creating new pages to save time
-def page_template(root, data, width, height):
+def page_template(root, data):
 
     # clears the root
     for canvas in root.winfo_children():
         canvas.destroy()
 
-    # creates a new empty canvas with the same width and height
-    canvas = Canvas(root)
+    # creates a new canvas
+    canvas = Canvas(root, width=data.width, height=data.height)
     canvas.configure(highlightthickness=0)
     canvas.pack()
 
     # draws the background
-    background = PhotoImage(file='images/cover.png')
-    background_label = Label(canvas, image=background)
-    background_label.image = background
-    background_label.place(x=width // 2, y=height // 2, anchor=CENTER)
+    canvas.create_image(data.width // 2, data.height // 2, image=data.cover)
 
     # a button with common configs edited
     btn = Button(canvas, text='BACK BUTTON', fg='green', bg="#%02x%02x%02x" % (50, 100, 200),
                  font='Times 50 bold italic underline', command=lambda: cover(root, data))
-    btn.place(x=width // 2, y=height // 4, anchor=CENTER)
+    btn.place(x=data.width // 2, y=data.height // 4, anchor=CENTER)
 
     # a text label that shows the content entered in the entry
     text_label = Label(canvas, fg='green', font='Georgia 30')
-    text_label.place(x=width // 2, y=height // 2, anchor=CENTER)
+    text_label.place(x=data.width // 2, y=data.height // 2, anchor=CENTER)
 
     # a string variable that stores the text in entry when enter button is clicked
     entry_input = StringVar(None, 'ENTER SOMETHING!')
 
     # entry widget
     entry = Entry(canvas, textvariable=entry_input)
-    entry.place(x=width // 2, y=height * 3 // 4, anchor=CENTER, height=20)
+    entry.place(x=data.width // 2, y=data.height * 3 // 4, anchor=CENTER, height=20)
 
     # button that helps display stored information
     btn_enter = Button(canvas, text='ENTER',
                        command=lambda: text_label.config(text='You Entered: ' + entry_input.get(), bg='red'))
-    btn_enter.place(x=width // 2, y=height * 3 // 4 + 20, anchor=CENTER, height=20)
+    btn_enter.place(x=data.width // 2, y=data.height * 3 // 4 + 20, anchor=CENTER, height=20)
 
     # returns to the cover
     btn_back = Button(canvas, text='Back', command=lambda: cover(root, data))
