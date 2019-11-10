c2cd84ca4b69098538e103719ce6c2d328d7e0af
Bryan Lim Cheng Yee <bryan123bryan@Bryans-MacBook-Pro.local>
2019-11-10 08:51:09 +0800

    added a dictionary for the menu

---
+++ Menu Dictionary.py
@@ -0,0 +1,17 @@
+menu_dict = { "McDonald's": {"Breakfast": {"Sausage McMuffin": "$5.00", "Big Breakfast": "$7.00"},
+                        "Lunch": {"Big Mac": "$7.85", "Double McSpicy": "$9.55"},
+                        "Dinner": {"The Original Angus": "$8.50", "Double Cheeseburger": "$6.65"}},
+         
+         "Japanese Korean Delight": {"Breakfast": {"Oyako Don": "$4.80", "Chicken Udon": "$4.80"},
+                                     "Lunch": {"Hotplate Soba Set": "$5.50", "Hotplate Pork Set": "$5.80"},
+                                     "Dinner": {"Pork Don": "$5.30", "Beef Don": "$6.00"}},
+         
+         "The Sandwich Guys": {"Breakfast": {"Barbeque Pulled Pork": "$6.00", "Philly Cheesesteak": "$7.00", "Mexicana Grilled Chicken": "$6.00"},
+                               "Lunch": {"Peri Peri Chicken": "$6.00", "Cubano": "$7.00", "Cowboy Burger": "$8.00"},
+                               "Dinner": {"Roast Beef": "$7.00", "The MLT (Veg)": "$6.00", "Roasted Eggplant and Cheese": "$7.00"}},
+         
+         "Cantonese Roast Duck": {"Breakfast": {"Roasted Duck Rice": "$3.30", "Roasted Duck Noodle": "$3:30"},
+                                  "Lunch": {"Char Siew Rice": "$2.80", "Char Siew Noodle": "$2.80"},
+                                  "Dinner": {"Roasted Meat Rice": "$2.80", "Char Siew Roasted Meat Noodle": "$4.00"}
+
+                                  }
